3953bbaaa78bdc6296e48b3b86889cbc90fb17b0
Paweł Krupa <pawel@krupa.net.pl>
2020-12-10 13:52:47 +0100

    Update test_latest.py
    Add any test so `latest` workflow won't exit with an error code

--- molecule/latest/tests/test_latest.py
+++ molecule/latest/tests/test_latest.py
@@ -1,5 +1,17 @@
+import pytest
 import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("files", [
+    "/etc/cortex/cortex.yml",
+    "/etc/systemd/system/cortex.service",
+    "/usr/local/bin/cortex-linux-amd64",
+])
+def test_files(host, files):
+    f = host.file(files)
+    assert f.exists
+    assert f.is_file
